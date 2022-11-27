c3db4b289c92d2c84fd7638d1a5eda19f0c2cbe5
SoozaV <s8za.92@gmail.com>
2022-11-27 01:50:18 -0600

    Testing prisma auth error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,4 +6,5 @@ declare global {
 
 export const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+//if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+global.prisma = prisma
